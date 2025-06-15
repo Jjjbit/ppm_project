3ca9e650c28fcb3a43520eba4e9906cf3873c2b2
daiji <jialu.dai@edu.unifi.it>
2025-06-15 18:26:59 +0200

    add home.html

--- ppm_project/urls.py
+++ ppm_project/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from home.views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('users/', include('users.urls')), # new
     path('users/', include('django.contrib.auth.urls')), #new
 
