b3f763a804a275c6f779ce6ccaf8277426ae8954
daiji <jialu.dai@edu.unifi.it>
2025-06-15 22:34:14 +0200

    rename home.html to profile.html

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import SignUpView, MyLoginView, profile_view, CustomPasswordChangeView, CustomPasswordChangeDoneView
+from .views import SignUpView, MyLoginView, ProfileView, CustomPasswordChangeView, CustomPasswordChangeDoneView, \
+    edit_profile_view, ProfileView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", MyLoginView.as_view(), name="login"),
-    path("profile/", profile_view, name="profile"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path('profile/edit-profile/', edit_profile_view, profile_view, name='edit_profile'),  # Assuming edit_profile_view is the same as profile_view for simplicity
     path('password-change/', CustomPasswordChangeView.as_view(), name='password_change'),
 
     path('password-change-done/', CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
