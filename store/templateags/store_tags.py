2abcce01e3347f4d6a63c0b3e3c6d870abccae2d
daiji <jialu.dai@edu.unifi.it>
2025-06-16 20:03:56 +0200

    delete store\templatetags

--- store/templateags/store_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from store.models import Category
-
-register = template.Library()
-
-@register.inclusion_tag('base.html')
-def category_list():
-    return {'categories': Category.objects.all()}
