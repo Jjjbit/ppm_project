7fe4733bee51984e08990b27fa86587aafad3f29
daiji <jialu.dai@edu.unifi.it>
2025-06-17 21:05:36 +0200

    add Store model

--- store/models.py
+++ store/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from users.models import CustomUser
+
+class Store(models.Model):
+    owner = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='store')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -10,18 +18,24 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    discount = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
     stock = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} - {self.price} - {self.seller.username}"
 
-    # def get_absolute_url(self):
-    #     return reverse("product_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.pk})
+
+    def get_image_url(self):
+        if self.image:
+            return self.image.url
+        return "https://placehold.co/600x400"
 
 
 class Order(models.Model):
