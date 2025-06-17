5cce14ea47b2e3b9b1e374187a6ec4acaf6045dc
daiji <jialu.dai@edu.unifi.it>
2025-06-17 21:10:03 +0200

    add url of store and add-product

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (StoreView, StoreCategoryProductsView)
+from .views import (StoreView, StoreCategoryProductsView, add_product)
 
 
 urlpatterns = [
-    path('', StoreView.as_view(), name='store_dashboard'),
+    path('<int:seller_id>/', StoreView.as_view(), name='store_dashboard'),
     path('<int:seller_id>/category/<int:category_id>/', StoreCategoryProductsView.as_view(), name='store_category_products'),
+    path('<int:seller_id>/add-product/', add_product, name='add_product'),
 ]
 
