610b038a99b8429b45b36d572cdea85f5d47e83f
daiji <jialu.dai@edu.unifi.it>
2025-06-17 21:15:11 +0200

    login as admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_store_manager', 'is_staff', 'is_active')
+    list_filter = ('is_store_manager', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_store_manager', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_store_manager', 'is_active', 'is_staff', 'is_superuser')}
+         ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
 # Register your models here.
