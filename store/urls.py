992c989dfb637476b15a4d313246c8b6d0453e0f
daiji <jialu.dai@edu.unifi.it>
2025-06-18 21:43:19 +0200

    add add_product

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import (StoreView, StoreCategoryProductsView, add_product)
-
+from .views import (StoreView, StoreCategoryProductsView, AddProductView,
+                    EditProductView, DeleteProductView)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('<int:seller_id>/', StoreView.as_view(), name='store_dashboard'),
     path('<int:seller_id>/category/<int:category_id>/', StoreCategoryProductsView.as_view(), name='store_category_products'),
-    path('<int:seller_id>/add-product/', add_product, name='add_product'),
+    # path('<int:seller_id>/add-product/', add_product, name='add_product'),
+    path('<int:seller_id>/add-product/', AddProductView.as_view(), name='add_product'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
