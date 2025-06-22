c7b13b483beeda8ec9934d19275bf22d6e3a3b44
daiji <jialu.dai@edu.unifi.it>
2025-06-22 20:53:53 +0200

    add edit-product, delete-product

--- store/urls.py
+++ store/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('<int:seller_id>/category/<int:category_id>/', StoreCategoryProductsView.as_view(), name='store_category_products'),
     # path('<int:seller_id>/add-product/', add_product, name='add_product'),
     path('<int:seller_id>/add-product/', AddProductView.as_view(), name='add_product'),
+    path('product/<int:pk>/edit/', EditProductView.as_view(), name='edit_product'),
+    path('product/<int:pk>/delete/', DeleteProductView.as_view(), name='delete_product'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
