4edee4ecfd80f110a86f0be8d6bc806e7438b3f8
daiji <jialu.dai@edu.unifi.it>
2025-06-26 17:45:15 +0200

    add url of editing StoreCategory and deleting StoreCAtegory

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import (StoreView, StoreCategoryProductsView, AddProductView,
-                    EditProductView, DeleteProductView)
+                    EditProductView, DeleteProductView, AddStoreCategoryView, EditStoreCategoryView, DeleteStoreCategoryView)
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('<int:seller_id>/', StoreView.as_view(), name='store_dashboard'),
     path('<int:seller_id>/category/<int:category_id>/', StoreCategoryProductsView.as_view(), name='store_category_products'),
-    # path('<int:seller_id>/add-product/', add_product, name='add_product'),
+    path('<int_seller_id>/add-store-category/', AddStoreCategoryView.as_view(), name='add_store_category'),
+    path('<int:seller_id>/edit-store-category/<int:pk>/', EditStoreCategoryView.as_view(), name='edit_store_category'),
+    path('<int:seller_id>/delete-store-category/<int:pk>/', DeleteStoreCategoryView.as_view(), name='delete_store_category'),
     path('<int:seller_id>/add-product/', AddProductView.as_view(), name='add_product'),
     path('product/<int:pk>/edit/', EditProductView.as_view(), name='edit_product'),
     path('product/<int:pk>/delete/', DeleteProductView.as_view(), name='delete_product'),
